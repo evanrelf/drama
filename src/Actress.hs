ddcafc9817055f82e66cae58de0a05337aa7bc96
Evan Relf <evan@evanrelf.com>
2021-02-11 20:09:55 -0800

    Initial implementation

--- src/Actress.hs
+++ src/Actress.hs
@@ -1,4 +1,66 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE LambdaCase #-}
+
+
 module Actress
-  (
+  ( -- * Types
+    Actor
+  , Address
+  , Mailbox
+
+    -- * Functions
+  , receive
+  , send
+  , spawn
+  , run
+  , loop
+
+    -- * Re-exports
+  , Ki.Scope
+  , Ki.wait
   )
 where
+
+import qualified Control.Concurrent.Chan.Unagi as Unagi
+import qualified Ki
+
+
+type Actor message = Address message -> Mailbox message -> Ki.Scope -> IO ()
+
+
+newtype Address message = Address (Unagi.InChan message)
+
+
+newtype Mailbox message = Mailbox (Unagi.OutChan message)
+
+
+receive :: Mailbox message -> IO message
+receive (Mailbox outChan) = Unagi.readChan outChan
+
+
+send :: Address message -> message -> IO ()
+send (Address inChan) message = Unagi.writeChan inChan message
+
+
+spawn :: Ki.Scope -> Actor message -> IO (Address message)
+spawn scope actor = do
+  (inChan, outChan) <- Unagi.newChan
+  let address = Address inChan
+  let mailbox = Mailbox outChan
+  Ki.fork_ scope (Ki.scoped \childScope -> actor address mailbox childScope)
+  pure address
+
+
+run :: Actor message -> IO ()
+run actor = do
+  (inChan, outChan) <- Unagi.newChan
+  let address = Address inChan
+  let mailbox = Mailbox outChan
+  Ki.scoped \childScope -> actor address mailbox childScope
+
+
+loop :: a -> (a -> IO (Maybe a)) -> IO ()
+loop x0 k = do
+  k x0 >>= \case
+    Just x -> loop x k
+    Nothing -> pure ()
---
+++ src/Actress/Demo.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE BlockArguments #-}
+
+module Actress.Demo where
+
+import Actress
+import Control.Monad (forever)
+import Prelude hiding (log)
+
+
+logger :: Actor String
+logger _self mailbox _scope = loop (1 :: Int) \count -> do
+  string <- receive mailbox
+  putStrLn (show count <> ": " <> string)
+  pure $ Just (count + 1)
+
+
+echo :: (String -> IO ()) -> Actor ()
+echo log _self _mailbox _scope = forever do
+  line <- getLine
+  if line == "ping" then
+    log "pong"
+  else
+    log line
+
+
+add1 :: (String -> IO ()) -> Actor (Address Int, Int)
+add1 log _self mailbox scope = forever do
+  log "[add1] Waiting for request"
+  (returnAddress, number) <- receive mailbox
+  log "[add1] Received request"
+  log "[add1] Spawning worker"
+  spawn scope (add1Worker log number returnAddress)
+
+
+add1Worker :: (String -> IO ()) -> Int -> Address Int -> Actor ()
+add1Worker log number returnAddress _self _mailbox _scope = do
+  log "[add1Worker] Started"
+  send returnAddress (number + 1)
+  log "[add1Worker] Replied"
+
+
+program :: Actor Int
+program self mailbox scope = do
+  putStrLn "[main] START"
+
+  putStrLn "[main] Spawning logger"
+  loggerAddress <- spawn scope logger
+
+  let log = send loggerAddress
+
+  putStrLn "[main] Spawning echo"
+  _ <- spawn scope (echo log)
+
+  log "[main] Spawning add1"
+  add1Address <- spawn scope (add1 log)
+
+  log "[main] Sending number to add1"
+  send add1Address (self, 1)
+  two <- receive mailbox
+  log ("[main] Received response from add1: " <> show two)
+
+  log "[main] FINISH"
+  wait scope
+
+
+main :: IO ()
+main = run program
