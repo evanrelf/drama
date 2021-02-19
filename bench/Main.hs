0fe19127bad7b110f508db62ac1e52f5c4a152c3
Evan Relf <evan@evanrelf.com>
2021-02-18 21:20:42 -0800

    Add simple benchmark

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,5 +1,41 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE NumericUnderscores #-}
+
+
 module Main (main) where
+
+import Criterion.Types
+import Criterion.Main
+import Starring
+import Prelude hiding (log, max, sum)
 
 
 main :: IO ()
-main = putStrLn "TODO"
+main = defaultMainWith defaultConfig{timeLimit = 30, verbosity = Verbose}
+  [ bgroup "benchCounterSummer"
+      [ bench "1_000_000" $ nfIO (benchCounterSummer 1_000_000)
+      ]
+  ]
+
+
+benchCounterSummer :: Int -> IO ()
+benchCounterSummer max = run do
+  summerAddress <- spawn (summer max)
+  _ <- spawn (counter max summerAddress)
+  wait
+
+
+summer :: Int -> Actor Int ()
+summer max = loop (max, 0) \(count, sum) -> do
+  n <- receive
+  if count > 0
+    then continue (count - 1, sum + n)
+    else exit ()
+
+
+counter :: Int -> Address Int -> Actor msg ()
+counter max summerAddress = loop 0 \count -> do
+  send summerAddress count
+  if count < max
+    then continue (count + 1)
+    else exit ()
