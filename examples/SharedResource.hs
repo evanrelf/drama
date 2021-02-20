cf30fe5309921a4e3dc2689c2cd872d169dac1f3
Evan Relf <evan@evanrelf.com>
2021-02-20 14:41:34 -0800

    Document examples

--- examples/SharedResource.hs
+++ examples/SharedResource.hs
@@ -13,20 +13,29 @@ import Prelude hiding (log)
 
 main :: IO ()
 main = run do
+  -- Spawn `logger` actor, which starts waiting for requests.
   loggerAddress <- spawn logger
 
+  -- Spawn two noisy actors, `fizzBuzz` and `navi`, which want to constantly
+  -- print to the console. Instead of running `putStrLn`, they will send
+  -- messages to `logger`.
   _ <- spawn (fizzBuzz loggerAddress)
   _ <- spawn (navi loggerAddress)
 
+  -- Block `main` thread forever.
   wait
 
 
+-- | Actor which encapsulates access to the console (a shared resource). By
+-- sending log messages to `logger`, instead of running `putStrLn` directly, we
+-- can avoid interleaving logs from actors running in parallel.
 logger :: Actor String ()
 logger = forever do
   string <- receive
   liftIO $ putStrLn string
 
 
+-- | Silly example actor which wants to print to the console
 fizzBuzz :: Address String -> Actor () ()
 fizzBuzz loggerAddress = do
   let log = send loggerAddress
@@ -37,14 +46,19 @@ fizzBuzz loggerAddress = do
        | n `mod`  5 == 0 -> log "Buzz"
        | otherwise       -> log (show n)
 
+    -- Delay included for nicer (slow) output
     liftIO $ threadDelay 500_000
 
     continue (n + 1)
 
 
+-- | Silly example actor which wants to print to the console
 navi :: Address String -> Actor () ()
 navi loggerAddress = do
   let log = send loggerAddress
+
   forever do
     log "Hey, listen!"
+
+    -- Delay included for nicer (slow) output
     liftIO $ threadDelay 1_000_000
