9d5e81cc30e6450521c4dc674075db5d72138ea9
Evan Relf <evan@evanrelf.com>
2021-02-21 15:33:02 -0800

    Update `workers` example to use `Server`

--- examples/workers.hs
+++ examples/workers.hs
@@ -1,47 +1,48 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE EmptyCase #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Main (main) where
 
-import Control.Monad (replicateM, when)
 import Data.Function ((&))
 import Drama
 
 
 main :: IO ()
-main = run do
+main = run_ do
   -- Spawn `fib` process, which starts waiting for requests.
   fibAddr <- spawn fib
+
+  let ns = [200, 400, 600]
 
   -- Request fibonacci numbers from `fib`. `fib` will spawn three `fibWorker`s
   -- to do all the work in the background, and then send `main` their results
   -- once they finish.
-  self <- here
-  [200, 400, 600] & mapM_ \n -> send fibAddr (self, n)
+  fs <- mapM (call fibAddr . GetFibNumber) ns
 
-  -- Receive results sent back from the `fibWorker`s, and print them to the
-  -- console.
-  replicateM 3 receive >>= mapM_ \(n, f) ->
+  fs & mapM_ \(n, f) ->
     liftIO $ putStrLn ("Fibonacci number " <> show n <> " is " <> show f)
 
-  -- Tell `fib` to stop.
-  send fibAddr (self, -1)
 
-  -- Wait for `fib` to stop before exiting.
-  wait
+data FibMsg res where
+  GetFibNumber :: Int -> FibMsg (Int, Integer)
 
 
 -- | Process which immediately spawns and delegates to "worker" processes.
-fib :: Process (Address (Int, Integer), Int) ()
-fib = do
-  (responseAddr, n) <- receive
-  when (n >= 0) do
-    spawn_ (fibWorker responseAddr n)
-    fib
+fib :: Process (Envelope FibMsg) ()
+fib = forever do
+  envelope <- receive
+  spawn_ (fibWorker envelope)
 
 
--- | "Worker" process responsible for doing the real, time-consuming work.
-fibWorker :: Address (Int, Integer) -> Int -> Process Void ()
-fibWorker responseAddr n = send responseAddr (n, fibs !! n)
+-- | "Worker" process responsible for doing the real, time-consuming work. Dies
+-- after sending its result to the return address.
+fibWorker :: Envelope FibMsg -> Process Void ()
+fibWorker = \case
+  Cast msg -> case msg of {}
+  Call returnAddr (GetFibNumber n) ->
+    send returnAddr (n, fibs !! n)
 
 
 -- | Infinite list of fibonacci numbers.
