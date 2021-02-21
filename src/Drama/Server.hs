31794286fcd34fef9f29bdaf4ef8a07427da9dcf
Evan Relf <evan@evanrelf.com>
2021-02-21 12:09:33 -0800

    Implement `Server` abstraction

---
+++ src/Drama/Server.hs
@@ -0,0 +1,21 @@
+-- |
+-- Module:     Drama.Server
+-- Stability:  experimental
+-- License:    BSD-3-Clause
+-- Copyright:  © 2021 Evan Relf
+-- Maintainer: evan@evanrelf.com
+--
+-- TODO
+--
+
+module Drama.Server
+  ( Server
+  , Envelope
+  , start
+  , cast
+  , call
+  , handle
+  )
+where
+
+import Drama.Server.Internal
