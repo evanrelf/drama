cde37461d5e758640dc66a238137c14c85e218ed
Evan Relf <evan@evanrelf.com>
2021-02-23 21:28:16 -0800

    Move `hie` output into `drama-stan`

--- nix/haskell-packages.nix
+++ nix/haskell-packages.nix
@@ -10,13 +10,4 @@ import ./lib/override-haskell-packages.nix {
   overrideCabal = {
     "ki" = _: { broken = false; };
   };
-
-  overrideAttrs = {
-    "drama" = old: {
-      outputs = (old.outputs or []) ++ [ "hie" ];
-      postBuild = (old.postBuild or "") + ''
-        cp -R .hie $hie
-      '';
-    };
-  };
 } pkgsFinal pkgsPrev
