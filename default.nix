7dc064004b0542e41f88d31611b218725e7ed6b1
Evan Relf <evan@evanrelf.com>
2021-02-23 20:05:11 -0800

    Accept `pkgs` as argument in `{default,shell}.nix`

--- default.nix
+++ default.nix
@@ -1,7 +1,6 @@
-let
-  pkgs = import ./nix/pkgs.nix;
+{ pkgs ? import ./nix/pkgs.nix }:
 
-in {
+{
   drama = pkgs.haskellPackages.drama;
 
   drama-hlint = pkgs.callPackage ./nix/drama-hlint.nix {};
