c65241320c81453946f422a1b1f08a587e35fbba
Evan Relf <evan@evanrelf.com>
2022-07-16 22:06:11 -0700

    Update Nixpkgs pin

--- nix/haskell-packages.nix
+++ nix/haskell-packages.nix
@@ -6,8 +6,4 @@ import ./lib/override-haskell-packages.nix {
   packages = {
     "drama" = pkgsPrev.nix-gitignore.gitignoreSource [ ../.nixignore ] ../.;
   };
-
-  overrideCabal = {
-    "ki" = _: { broken = false; };
-  };
 } pkgsFinal pkgsPrev
--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,9 +1,9 @@
 args:
 
 let
-  # master on 2021-02-11
-  rev = "0f3b7ea1dd1605c5a7049ce596f9f24e72f52a0f";
-  sha256 = "15mzdjq43rh5r71z3838jg7lk7jzymxzavlvc7c7nkfcvsw3bgr5";
+  # master on 2022-07-16
+  rev = "ff0e4cfbcd7881f50caa788ebd488e2e6f782431";
+  sha256 = "1s0gx9ikh36h2w6pj9jjll7i32hzzmzmrax9w9jf405rd7822qx3";
 
   nixpkgs = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
